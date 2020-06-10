fc8e66ac753e249ba4bb7e83967e28c72d42052f
Irena <irenejpopova6@gmail.com>
2020-06-10 16:26:24 +0200

    error className added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom;'
 
 export default function Error() {
-  return <h1>error page</h1>;
+
+  return (
+    <section className="error-page container">
+      <div className="error-container">
+        <h1>oops! it is  a dead end</h1>
+        <link to="/" className="btn btn-primary">
+          Back Home
+      </link>
+      </div>
+    </section>
+  );
 }
