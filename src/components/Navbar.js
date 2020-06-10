25f0b18d8b347f075501707964ac7b1cd5933d45
Irena <irenejpopova6@gmail.com>
2020-06-10 16:18:13 +0200

    classNames added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { link } from 'react-router-dom';
+
 
 export default function Navbar() {
-  return <h1>navbar component</h1>;
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <img src={logo} alt="cocktail db logo" className="logo" />
+        <ul className="nav-links">
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
